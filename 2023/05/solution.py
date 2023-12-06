356fdb9c2b5a6348db85a5b701779d6b404cccf3
Reagan Chisholm <reaganchisholm@gmail.com>
2023-12-05 21:04:38 -0500

    day 05 p2

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -55,10 +55,9 @@ def part_1():
     for m in maps:
         map_group = []
         for l in m:
-            dest, src, rgn = [int(n) for n in l.split()]
-            start = src
-            end = src + (rgn - 1)
-            diff = abs(dest - src)
+            dest, start, rgn = [int(n) for n in l.split()]
+            end = start + (rgn - 1)
+            diff = abs(dest - start)
             map_group.append([start, end, dest, diff])
         processed_maps.append(map_group)
 
@@ -73,4 +72,56 @@ def part_1():
 
     print(f"Part 1 --- {min(locations)}")
 
+# This is a very slow brute force solution, but it works :shrug:
+def part_2():
+    lines = get_lines(False)
+    seeds = [int(s) for s in lines[0].split(":")[1].strip().split()]
+    lines = lines[1:] # Remove seeds line
+    lines = lines[::-1] # Reverse lines
+    maps = []
+    processed_maps = []
+    range_of_seeds = []
+
+    for i in range(0, len(seeds), 2):
+        min_max = [seeds[i], seeds[i] + seeds[i+1] - 1]
+        range_of_seeds.append(min_max)
+
+    for l in lines:
+        maps.append(l.split(":")[1].strip().splitlines())
+
+    for m in maps:
+        map_group = []
+        for l in m:
+            start, dest, rgn = [int(n) for n in l.split()]
+            end = start + (rgn - 1)
+            diff = abs(dest - start)
+            map_group.append([start, end, dest, diff])
+        processed_maps.append(map_group)
+
+    location = 0
+    lowest_location = 0
+
+    while(True):
+        start = location
+
+        for m in processed_maps:
+            for g in m:
+                if(start >= g[0] and start <= g[1]):
+                    diff = start - g[0]
+                    start = g[2] + diff
+                    break
+
+        for r in range_of_seeds:
+            if start >= r[0] and start <= r[1]:
+                lowest_location = location; 
+                break
+
+        if(lowest_location != 0):
+            break
+        else:
+            location += 1
+
+    print(f"Part 2 --- {lowest_location}")
+
 part_1()
+part_2()
