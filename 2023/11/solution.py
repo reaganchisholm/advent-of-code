00a4fa957f887d0b60b4d445fb4bbc041e0f57dc
Reagan Chisholm <reaganchisholm@gmail.com>
2023-12-11 22:48:16 -0500

    refactor day 11

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -18,93 +18,54 @@ def get_lines(use_test_data):
             lines = f.read().splitlines()
             return lines
 
+def find_empty_rows_cols(grid):
+    empty_rows = {y for y, row in enumerate(grid) if '#' not in row}
+    empty_cols = {x for x in range(len(grid[0])) if all(row[x] != '#' for row in grid)}
+    return empty_rows, empty_cols
+
+def manhattan_distance(point1, point2, empty_rows, empty_cols, amount = 1):
+    x_distance = abs(point2[0] - point1[0])
+    y_distance = abs(point2[1] - point1[1])
+
+    for row in empty_rows:
+        if min(point1[1], point2[1]) < row < max(point1[1], point2[1]):
+            y_distance += amount
+
+    for col in empty_cols:
+        if min(point1[0], point2[0]) < col < max(point1[0], point2[0]):
+            x_distance += amount
+
+    return x_distance + y_distance
+
 def part_1():
     lines = get_lines(False)
-    grid = build_grid(lines)
+    grid = [line for line in lines]
     empty_rows, empty_cols = find_empty_rows_cols(grid)
-    galaxy_coords = get_galaxy_coords(grid)
-
+    galaxy_coords = [(x, y) for y in range(len(grid)) for x in range(len(grid[y])) if grid[y][x] == '#'] 
     total_distance = 0
 
     for i in range(len(galaxy_coords)):
         for j in range(i + 1, len(galaxy_coords)):
-            distance = manhattan_distance_p1(galaxy_coords[i], galaxy_coords[j], empty_rows, empty_cols)
+            distance = manhattan_distance(galaxy_coords[i], galaxy_coords[j], empty_rows, empty_cols)
             total_distance += distance
 
     print(f"Part 1 --- {total_distance}")
 
 def part_2():
     lines = get_lines(False)
-    grid = build_grid(lines)
+    grid = [line for line in lines]
     empty_rows, empty_cols = find_empty_rows_cols(grid)
-    galaxy_coords = get_galaxy_coords(grid)
+    galaxy_coords = [(x, y) for y in range(len(grid)) for x in range(len(grid[y])) if grid[y][x] == '#'] 
 
     total_distance = 0
 
     for i in range(len(galaxy_coords)):
         for j in range(i + 1, len(galaxy_coords)):
-            distance = manhattan_distance_p2(galaxy_coords[i], galaxy_coords[j], empty_rows, empty_cols)
+            distance = manhattan_distance(galaxy_coords[i], galaxy_coords[j], empty_rows, empty_cols, 999999)
             total_distance += distance
 
     print(f"Part 2 --- {total_distance}")
 
-def build_grid(lines):
-    grid = []
-    for line in lines:
-        grid.append(list(line))
-    return grid
-
-def get_galaxy_coords(grid):
-    galaxy_coords = []
-
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            if(grid[y][x] == '#'):
-                galaxy_coords.append((x,y))
-
-    return galaxy_coords 
-
-def find_empty_rows_cols(grid):
-    empty_rows = set(range(len(grid)))
-    empty_cols = set(range(len(grid[0])))
-
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == '#':
-                if y in empty_rows:
-                    empty_rows.remove(y)
-                if x in empty_cols:
-                    empty_cols.remove(x)
-
-    return empty_rows, empty_cols
-
-def manhattan_distance_p1(point1, point2, empty_rows, empty_cols):
-    x_distance = abs(point2[0] - point1[0])
-    y_distance = abs(point2[1] - point1[1])
-
-    for row in empty_rows:
-        if min(point1[1], point2[1]) < row < max(point1[1], point2[1]):
-            y_distance += 1
-
-    for col in empty_cols:
-        if min(point1[0], point2[0]) < col < max(point1[0], point2[0]):
-            x_distance += 1
-
-    return x_distance + y_distance
-
-def manhattan_distance_p2(point1, point2, empty_rows, empty_cols):
-    x_distance = abs(point2[0] - point1[0])
-    y_distance = abs(point2[1] - point1[1])
-
-    for row in empty_rows:
-        if min(point1[1], point2[1]) < row < max(point1[1], point2[1]):
-            y_distance += 999999
-
-    for col in empty_cols:
-        if min(point1[0], point2[0]) < col < max(point1[0], point2[0]):
-            x_distance += 999999
-
-    return x_distance + y_distance
 
 part_1()
 part_2()
