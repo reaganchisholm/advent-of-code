301863fac650d5a42c9c2de2f2b78068caed1167
Reagan Chisholm <reaganchisholm@gmail.com>
2024-12-08 16:24:00 -0500

    start day05p02

--- 2024/05/main.go
+++ 2024/05/main.go
@@ -86,6 +86,77 @@ func part1() {
 }
 
 func part2() {
+	data := `47|53
+	97|13
+	97|61
+	97|47
+	75|29
+	61|13
+	75|53
+	29|13
+	97|29
+	53|29
+	61|53
+	97|53
+	61|29
+	47|13
+	75|47
+	97|75
+	47|61
+	75|61
+	47|29
+	75|13
+	53|13
+
+	// 75,47,61,53,29
+	// 97,61,53,29,13
+	// 75,29,13
+	// 75,97,47,61,53
+	// 61,13,29
+	// 97,13,75,29,47`
+
+	// dataBytes, err := os.ReadFile("input.txt")
+	// if err != nil {
+	// 	fmt.Println("Error reading file:", err)
+	// 	return
+	// }
+
+	// data := string(dataBytes)
+	instr, data := strings.Split(data, "\n\n")[0], strings.Split(data, "\n\n")[1]
+	var total int
+
+	// Set up the instructions
+	instructions := make(map[int][]int)
+	for _, line := range strings.Split(instr, "\n") {
+		strOne, strTwo := strings.Split(line, "|")[0], strings.Split(line, "|")[1]
+		numOne, _ := strconv.Atoi(strOne)
+		numTwo, _ := strconv.Atoi(strTwo)
+
+		instructions[numOne] = append(instructions[numOne], numTwo)
+	}
+
+	// Set up the data
+	dataArr := make([][]int, 0)
+	for _, line := range strings.Split(data, "\n") {
+		nums := strings.Split(line, ",")
+		numArr := make([]int, 0)
+		for _, num := range nums {
+			numInt, _ := strconv.Atoi(num)
+			numArr = append(numArr, numInt)
+		}
+		dataArr = append(dataArr, numArr)
+	}
+
+	// Now we test the data
+	for _, data := range dataArr {
+		if isValid(instructions, data) == false {
+			fixedLine := fixLine(instructions, data)
+			middleNum := fixedLine[len(fixedLine)/2]
+			total = total + middleNum
+		}
+	}
+
+	println("Part 2 --------- ", total)
 }
 
 func isValid(instructions map[int][]int, data []int) bool {
@@ -99,6 +170,14 @@ func isValid(instructions map[int][]int, data []int) bool {
 	}
 
 	return isValid
+}
+
+func fixLine(instructions map[int][]int, data []int) []int {
+	fixedLine := make([]int, 0)
+	firstNum := data[0]
+	fixedLine = append(fixedLine, firstNum)
+
+	return fixedLine
 }
 
 func Contains[T comparable](s []T, e T) bool {
